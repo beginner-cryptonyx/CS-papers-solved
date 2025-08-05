df6d006da6b768fe1cbf2b58c69f9004b6d9216b
Crypto <mayanksethi242008@gmail.com>
2025-08-05 17:31:23 +0530

    Completed question 1

--- 2022/May June/p41/Question1_J2022.py
+++ 2022/May June/p41/Question1_J2022.py
@@ -21,7 +21,29 @@ def ReadHighScores():
 def OutputHighScores():
     for player, score in player_data:
         print(f"{player} {score}")
-        
+
+def InsertPlayer(player, score):
+    global player_data
+    for i in range(10):
+        if score >= int(player_data[i][1]):
+            player_data.insert(i, [player,str(score)])
+            break
+    player_data = player_data[:10]
+
+def WriteTopTen():
+    global player_data
+    try: 
+        with open("/2022/May June/p41/NewHighScore.text", "w") as file:
+            content = ''
+            for player in player_data:
+                content += f"{player[0]}\n{player[1]}"
+            file.write(content)
+            file.close()
+    except Exception as Error:
+        print(f"An error occured, Traceback: {Error}")                
+                
+
+
 ReadHighScores()
 OutputHighScores()
 
@@ -33,3 +55,9 @@ while len(player_name) != 3:
 
 while int_score < 1 or int_score > 100000:
     int_score = int(input("Enter an integer score that is between 1 and 100,000 inclusive: "))
+
+
+InsertPlayer(player_name, int_score)
+
+
+OutputHighScores()
